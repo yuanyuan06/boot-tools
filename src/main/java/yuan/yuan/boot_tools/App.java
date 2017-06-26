fad6453693115edf244885b13ed9ed72020ba4b1
yuan.yuan1 <yuan.yuan1@WCGZ-DZ-3651.baozun.cn>
2017-06-26 11:29:42 +0800

    init

---
+++ src/main/java/yuan/yuan/boot_tools/App.java
@@ -0,0 +1,13 @@
+package yuan.yuan.boot_tools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
