24fafc43bed49a1798def248fe9f2ed253838819
yuan.yuan1 <yuan.yuan1@WCGZ-DZ-3651.baozun.cn>
2017-07-11 15:11:25 +0800

    aviator demo

---
+++ src/test/java/yuan/yuan/boot_tools/aviator/AviatorEngineTest.java
@@ -0,0 +1,19 @@
+package yuan.yuan.boot_tools.aviator;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import com.googlecode.aviator.AviatorEvaluator;
+import com.googlecode.aviator.Options;
+
+public class AviatorEngineTest {
+
+    @Test
+    public void testEcute(){
+        // 浮点解析为 BigDecimal
+        AviatorEvaluator.setOption(Options.ALWAYS_USE_DOUBLE_AS_DECIMAL, true);
+        BigDecimal result = (BigDecimal) AviatorEvaluator.exec("0.1+0.2");
+        System.out.println(result);
+    }
+}
