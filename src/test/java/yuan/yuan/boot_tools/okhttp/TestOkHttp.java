664a2723e1492263f0d0cbc895d7616d1003108f
yuan.yuan1 <yuan.yuan1@WCGZ-DZ-3651.baozun.cn>
2017-07-13 18:57:28 +0800

    omit

---
+++ src/test/java/yuan/yuan/boot_tools/okhttp/TestOkHttp.java
@@ -0,0 +1,13 @@
+package yuan.yuan.boot_tools.okhttp;
+
+import org.junit.Test;
+
+import com.squareup.okhttp.OkHttpClient;
+
+public class TestOkHttp {
+
+    @Test
+    public void testOk(){
+        OkHttpClient client = new OkHttpClient();
+    }
+}
