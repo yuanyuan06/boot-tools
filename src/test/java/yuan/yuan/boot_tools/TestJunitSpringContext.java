582708a3a8a5e325a0f1e1f42c157d67d483845b
yuan.yuan1 <yuan.yuan1@WCGZ-DZ-3651.baozun.cn>
2017-07-27 12:45:14 +0800

    单元测试, testng & junit

---
+++ src/test/java/yuan/yuan/boot_tools/TestJunitSpringContext.java
@@ -0,0 +1,10 @@
+package yuan.yuan.boot_tools;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+@ContextConfiguration(locations={"classpath*:spring_junit_config.xml"})
+public class TestJunitSpringContext extends AbstractJUnit4SpringContextTests {
+
+    
+}
---
+++ src/test/java/yuan/yuan/boot_tools/TestNgSpringContext.java
@@ -0,0 +1,9 @@
+package yuan.yuan.boot_tools;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@ContextConfiguration(locations = { "classpath:spring-testng-config.xml"})
+public class TestNgSpringContext extends AbstractTestNGSpringContextTests{
+
+}
