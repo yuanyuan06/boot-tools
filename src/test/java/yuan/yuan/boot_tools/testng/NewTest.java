77f5bc89ea7e2cdadad450740c66fc99df69e931
yuan.yuan1 <yuan.yuan1@WCGZ-DZ-3651.baozun.cn>
2017-08-01 11:18:26 +0800

    testng demo

---
+++ src/test/java/yuan/yuan/boot_tools/testng/NewTest.java
@@ -0,0 +1,11 @@
+package yuan.yuan.boot_tools.testng;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	@Test()
+    public void testEmailGenerator() {
+		System.out.println("hello");
+
+    }
+}
