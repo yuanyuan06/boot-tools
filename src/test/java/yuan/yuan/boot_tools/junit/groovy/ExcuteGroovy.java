589631351585a96cff40bdb853972512781cdb0e
yuanyuan06 <18801761215@163.com>
2017-08-15 01:40:50 +0800

    ... groovy

--- src/test/java/yuan/yuan/boot_tools/junit/groovy/ExcuteGroovy.java
+++ src/test/java/yuan/yuan/boot_tools/junit/groovy/ExcuteGroovy.java
@@ -6,15 +6,27 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.scripting.ScriptSource;
+import org.springframework.scripting.groovy.GroovyScriptFactory;
 import org.springframework.scripting.support.ResourceScriptSource;
+import org.springframework.scripting.support.ScriptFactoryPostProcessor;
+import org.w3c.dom.Element;
 
 import com.alibaba.fastjson.JSON;
 
+import groovy.util.GroovyScriptEngine;
 import yuan.yuan.boot_tools.groovy.RuleRoleService;
 import yuan.yuan.boot_tools.groovy.RuleTaskService;
 import yuan.yuan.boot_tools.groovy.TestSo;
@@ -91,4 +103,47 @@ public class ExcuteGroovy {
 				String scriptAsString = source.getScriptAsString();
 				System.out.println(scriptAsString);
 	}
+	
+	
+	@Test
+	public void tet() throws IOException {
+		
+		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		Resource resource = resolver.getResource("classpath:groovyScripts/RoleRuleTemplet.groovy");
+		System.out.println(resource.exists());
+		ScriptSource source = new ResourceScriptSource(resource);
+		
+		String scriptAsString = source.getScriptAsString();
+		
+		GenericBeanDefinition definition = new GenericBeanDefinition();
+		
+	}
+	
+	@Test
+	public void contextBean() {
+		ConfigurableApplicationContext configurableApplicationContext = (ConfigurableApplicationContext)context;
+		BeanDefinitionRegistry beanDefinitonRegistry = (BeanDefinitionRegistry) configurableApplicationContext.getBeanFactory();
+//		BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(serviceClass);
+//        beanDefinitionBuilder.addPropertyValue("servicename", service.getServiceName());
+//		beanDefinitonRegistry.registerBeanDefinition(beanName, beanDefinition);
+//		RootBeanDefinition bean = new RootBeanDefinition();
+//		BeanDefinitionHolder holder = new  BeanDefinitionHolder(bean, "beanId");
+//		BeanDefinitionReaderUtils.registerBeanDefinition(holder, registry);
+	}
+	
+	@Test
+	public void springContext() {
+		
+		GenericBeanDefinition bd = new GenericBeanDefinition();
+			bd.setBeanClassName("org.springframework.scripting.groovy.GroovyScriptFactory");
+			bd.setAttribute(ScriptFactoryPostProcessor.LANGUAGE_ATTRIBUTE, "groovy");
+			bd.setAutowireMode(0);
+			bd.setDependencyCheck(0);
+			bd.setAttribute(ScriptFactoryPostProcessor.REFRESH_CHECK_DELAY_ATTRIBUTE, Long.valueOf(2000));
+		BeanDefinitionHolder holder = new BeanDefinitionHolder(bd, "", null);
+		ConfigurableApplicationContext configurableApplicationContext = (ConfigurableApplicationContext)context;
+		BeanDefinitionRegistry beanDefinitonRegistry = (BeanDefinitionRegistry) configurableApplicationContext.getBeanFactory();
+		BeanDefinitionReaderUtils.registerBeanDefinition(holder, beanDefinitonRegistry);
+	}
+	
 }
