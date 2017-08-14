46caadf390f8cf96fb05ac7d64dfbaeba54fdb6f
yuan.yuan1 <yuan.yuan1@WCGZ-DZ-3651.baozun.cn>
2017-08-14 15:25:46 +0800

    ...

--- src/main/java/yuan/yuan/boot_tools/groovy/script/RuleTaskTemplet.groovy
+++ src/main/java/yuan/yuan/boot_tools/groovy/script/RuleTaskTemplet.groovy
@@ -8,26 +8,7 @@ import yuan.yuan.boot_tools.groovy.TestReturn
 class RuleTaskTemplet implements RuleTaskService{
 
 	@Override
-	public String createTask(def bill) {
-//		if(so instanceof TestSo) {
-//			TestSo ts = so;
-//			ts.setField1("TestSo")
-//			ts.with {
-//				if(field1 == "TestSo" || field2 == "2") {
-//					"new";
-//				}else {
-//					"hang"
-//				}
-//			}
-//		}else if(so instanceof TestSoLine) {
-//			TestSoLine ts = so;
-//			ts.setFieldLine1("TestSoLine")
-//			ts.getFieldLine1();
-//		}else {
-//			println "IllegalArgumentException";
-//			throw new IllegalArgumentException();
-//		}
-		
+	public String createTask(bill) {
 		switch(bill) {
 			case TestSo:
 				def status 
